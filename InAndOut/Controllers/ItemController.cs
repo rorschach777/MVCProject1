8a44a24ad5350550496fd6582991b3e066b79c1f
rorschach777 <msweitzer1987@gmail.com>
2021-09-14 17:07:28 -0400

    Item Controller Added

---
+++ InAndOut/Controllers/ItemController.cs
@@ -0,0 +1,27 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        [Route("Item")]
+        [Route("Item/{id?}")]
+        public IActionResult Index()
+        {
+            IEnumerable<Item> ObjList = _db.Items;
+            return View(ObjList);
+        }
+    }
+}
