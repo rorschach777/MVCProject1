50c42056c4c29ac09981f6df8608c1823f871b4f
rorschach777 <msweitzer1987@gmail.com>
2021-09-16 07:35:43 -0400

    Added Expenses Controller and Forms

---
+++ InAndOut/Controllers/ExpenseController.cs
@@ -0,0 +1,40 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public ExpenseController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var model = _db.Expenses;
+            return View(model);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExpense(Expense expense)
+        {
+            _db.Expenses.Add(expense);
+            _db.SaveChanges();
+            return Redirect("Index");
+        }
+    }
+}
--- InAndOut/Data/ApplicationDBContext.cs
+++ InAndOut/Data/ApplicationDBContext.cs
@@ -15,5 +15,7 @@ namespace InAndOut.Data
         }
         // These are the tables
         public DbSet<Item> Items { get; set; }
+
+        public DbSet<Expense> Expenses { get; set; }
     }
 }
