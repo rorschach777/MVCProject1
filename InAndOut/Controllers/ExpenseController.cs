669b1a63fa994c96eae35adf8c07232ac33f1246
rorschach777 <msweitzer1987@gmail.com>
2021-09-24 14:47:02 -0400

    Expenses - Update and Delete

--- InAndOut/Controllers/ExpenseController.cs
+++ InAndOut/Controllers/ExpenseController.cs
@@ -22,7 +22,7 @@ namespace InAndOut.Controllers
         }
 
         [HttpGet]
-    
+
         public IActionResult Create()
         {
             return View();
@@ -40,7 +40,77 @@ namespace InAndOut.Controllers
                 return Redirect("Index");
             }
             return View(expense);
-    
+
+        }
+
+
+        // Post Delete
+        [HttpGet]
+        public IActionResult Update(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense expense)
+        {
+            // Server side 
+            if (ModelState.IsValid)
+            {
+                // Checks the data attributes on the model
+                _db.Expenses.Update(expense);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(expense);
+
+        }
+
+        // Post Delete
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+  
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        // Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Expenses.Find(id);
+            if (obj != null)
+            {
+                _db.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); 
+            }
+            else {
+                return NotFound();
+            }
         }
     }
 }
